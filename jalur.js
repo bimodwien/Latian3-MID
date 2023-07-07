4d03abd6c73fd843114668326523f0beaa8f5218
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-07-07 13:58:18 +0700

    nambahin fitur isEnd

--- jalur.js
+++ jalur.js
@@ -6,13 +6,15 @@ const data = [
 const value = []
 
 for (let i = 0; i < data[0].length; i++){
-    const isEnd = true
+    let isEnd = true
     for (let j = 0; j < data.length ; j++) {
         if(data[j][i] === 0) {
             console.log(`Jalur ${j+1}`)
+            isEnd = false
             break
         }
     }
+    if (isEnd) break
 }
 
 // console.log(value)
